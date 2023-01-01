8a1e6e44907456d4909b59156a1ef78c948352f7
Scrib Lee <scriblee11@gmail.com>
2023-01-01 18:00:53 -0500

    added alphabetize function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 
 let pokemon_container = document.getElementById("pokemon_container");
-let kanto_pokemon_names = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran', 'Nidorina', 'Nidoqueen', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'Farfetch’d', 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak'];
+let kanto_pokemon_names = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard', 'Squirtle', 'Wartortle', 'Blastoise', 'Caterpie', 'Metapod', 'Butterfree', 'Weedle', 'Kakuna', 'Beedrill', 'Pidgey', 'Pidgeotto', 'Pidgeot', 'Rattata', 'Raticate', 'Spearow', 'Fearow', 'Ekans', 'Arbok', 'Pikachu', 'Raichu', 'Sandshrew', 'Sandslash', 'Nidoran♀', 'Nidorina', 'Nidoqueen', 'Nidoran♂', 'Nidorino', 'Nidoking', 'Clefairy', 'Clefable', 'Vulpix', 'Ninetales', 'Jigglypuff', 'Wigglytuff', 'Zubat', 'Golbat', 'Oddish', 'Gloom', 'Vileplume', 'Paras', 'Parasect', 'Venonat', 'Venomoth', 'Diglett', 'Dugtrio', 'Meowth', 'Persian', 'Psyduck', 'Golduck', 'Mankey', 'Primeape', 'Growlithe', 'Arcanine', 'Poliwag', 'Poliwhirl', 'Poliwrath', 'Abra', 'Kadabra', 'Alakazam', 'Machop', 'Machoke', 'Machamp', 'Bellsprout', 'Weepinbell', 'Victreebel', 'Tentacool', 'Tentacruel', 'Geodude', 'Graveler', 'Golem', 'Ponyta', 'Rapidash', 'Slowpoke', 'Slowbro', 'Magnemite', 'Magneton', 'Farfetch’d', 'Doduo', 'Dodrio', 'Seel', 'Dewgong', 'Grimer', 'Muk', 'Shellder', 'Cloyster', 'Gastly', 'Haunter', 'Gengar', 'Onix', 'Drowzee', 'Hypno', 'Krabby', 'Kingler', 'Voltorb', 'Electrode', 'Exeggcute', 'Exeggutor', 'Cubone', 'Marowak'];
 let kanto_pokemon = [];
 
 
@@ -49,6 +49,16 @@ const removePokemonButton = (i) =>{
     pokemonButton.classList.add("pokemonHide");
 }
 
+const deleteAllPokemonButtons = () =>{
+    let pokemon_container = document.getElementById("pokemon_container");
+    for (i=0; i<50; i++){
+        let pokemonButton = document.getElementById(i+1);
+
+        pokemon_container.removeChild(pokemonButton);
+
+    }
+}
+
 const addPokemonButton = (i) =>{
     let pokemonButton = document.getElementById(i+1);
     pokemonButton.classList.remove("pokemonHide");
@@ -66,12 +76,46 @@ for (i=0; i<50; i++){
 }
 
 const alphabetical = () =>{
+    //get all current showing elements and keep them in an array
+    let showingPokemon = [];
+    for (i=0; i<50; i++){
+        html_pokemon = document.getElementById(i+1);
+        //if the pokemon is showing in the container then push it's id in the stored array
+        if(html_pokemon.classList.contains("pokemon")){
+            showingPokemon.push(html_pokemon.id)
+        }
+    }
+    console.log(showingPokemon);
+
+    deleteAllPokemonButtons(); //deletes all current pokemon so we can make new pokemon display
+
+    
+    //create new array duplicating old one so the original isn't changed by sort
+    let alpha_kanto_pokemon_names = Array.from(kanto_pokemon_names);
+    //alphabetizes the duplicate names array
+    alpha_kanto_pokemon_names.sort();
+
+
+        for(pokemonAlphaIndex in alpha_kanto_pokemon_names){
+            
+            for (pokemonShowingIndex in showingPokemon){
+                let showingID = showingPokemon[pokemonShowingIndex];
+                if(alpha_kanto_pokemon_names[pokemonAlphaIndex] == kanto_pokemon_names[showingID-1]){   
+                    
+                    console.log(kanto_pokemon_names[showingID-1]); 
+                    createPokemonButton(showingID-1);        
+                }
+        }
+        }
+
+
+
 
 }
 
 document.getElementById("search_filter").addEventListener("input",function(){
+    resetPokemonDisplay();
     for (i=0; i<50; i++){
-        console.log(document.getElementById(i+1).name.toLowerCase());
     
         let pokemonName = (document.getElementById(i+1).name.toLowerCase());
         if(!(pokemonName.includes(this.value))){
@@ -84,3 +128,17 @@ document.getElementById("search_filter").addEventListener("input",function(){
     
 })
 
+const hideAllPokemon = () =>{
+    for (i=0; i<50; i++){
+        removePokemonButton(i);
+    }
+}
+
+const resetPokemonDisplay = () =>{
+    deleteAllPokemonButtons();
+    for (i=0; i<50; i++){
+        createPokemonButton(i);
+        addPokemonButton(i);
+    }
+    
+}
